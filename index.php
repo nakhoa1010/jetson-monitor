181d11b05f97cf656ebdd394bb293876dbab0f4c
nakhoa1010 <91071159+nakhoa1010@users.noreply.github.com>
2023-08-24 11:17:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,62 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>System Stats</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <title>System Stats Graph</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <style>
+        canvas {
+            max-width: 800px;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body>
-    <h1>System Stats</h1>
-    <h1>An ngu vãi lồn</h1>
-    <div id="stats">
-        <!-- Display data here -->
-    </div>
+    <canvas id="statsChart"></canvas>
 
     <script>
-        function updateStats() {
-            $.ajax({
-                url: 'get_stats.php', // PHP script to fetch data
-                success: function(data) {
-                    $('#stats').html(data);
+        var ctx = document.getElementById('statsChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [],
+                datasets: [{
+                    label: 'CPU Percent',
+                    data: [],
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1,
+                    fill: false
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'second',
+                            displayFormats: {
+                                second: 'HH:mm:ss'
+                            }
+                        }
+                    }
                 }
-            });
+            }
+        });
+
+        function fetchData() {
+            fetch('data.php')
+                .then(response => response.json())
+                .then(data => {
+                    data.forEach(entry => {
+                        chart.data.labels.push(entry.insertion_time);
+                        chart.data.datasets[0].data.push(entry.cpu_percent);
+                    });
+                    chart.update();
+                });
         }
 
-        // Initial load and periodic updates
-        updateStats();
-        setInterval(updateStats, 5000); // Update every 5 seconds
+        fetchData();
+        setInterval(fetchData, 5000); // Update every 5 seconds
     </script>
 </body>
 </html>
