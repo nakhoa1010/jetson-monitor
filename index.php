046eef7df2574012ccfe55e99b21f502d76ab1e5
nakhoa1010 <91071159+nakhoa1010@users.noreply.github.com>
2023-08-24 14:41:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>System Stats Graph</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <style>
-        canvas {
-            max-width: 800px;
-            margin: 0 auto;
-        }
-    </style>
+    <title>System Stats</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-    Nothing Here
+    <h1>System Stats</h1>
+    <div id="stats">
+        <!-- Display data here -->
+    </div>
+
+    <script>
+        function updateStats() {
+            $.ajax({
+                url: 'get_stats.php', // PHP script to fetch data
+                success: function(data) {
+                    $('#stats').html(data);
+                }
+            });
+        }
+
+        // Initial load and periodic updates
+        updateStats();
+        setInterval(updateStats, 5000); // Update every 5 seconds
+    </script>
 </body>
 </html>
