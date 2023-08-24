9a762f41f5a8bcc55488477f49daa5c6d8dcaf87
nakhoa1010 <91071159+nakhoa1010@users.noreply.github.com>
2023-08-24 14:01:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,52 +11,6 @@
     </style>
 </head>
 <body>
-    <canvas id="statsChart"></canvas>
-
-    <script>
-        var ctx = document.getElementById('statsChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: [],
-                datasets: [{
-                    label: 'CPU Percent',
-                    data: [],
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1,
-                    fill: false
-                }]
-            },
-            options: {
-                responsive: true,
-                scales: {
-                    x: {
-                        type: 'time',
-                        time: {
-                            unit: 'second',
-                            displayFormats: {
-                                second: 'HH:mm:ss'
-                            }
-                        }
-                    }
-                }
-            }
-        });
-
-        function fetchData() {
-            fetch('data.php')
-                .then(response => response.json())
-                .then(data => {
-                    data.forEach(entry => {
-                        chart.data.labels.push(entry.insertion_time);
-                        chart.data.datasets[0].data.push(entry.cpu_percent);
-                    });
-                    chart.update();
-                });
-        }
-
-        fetchData();
-        setInterval(fetchData, 5000); // Update every 5 seconds
-    </script>
+    Nothing Here
 </body>
 </html>
