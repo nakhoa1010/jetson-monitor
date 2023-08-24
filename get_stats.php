bc3052fdb0fb743ceb54ca16f415bce970d741ce
nakhoa1010 <91071159+nakhoa1010@users.noreply.github.com>
2023-08-24 10:29:05 +0700

    Add files via upload

---
+++ get_stats.php
@@ -0,0 +1,29 @@
+<?php
+$server = "jetson.database.windows.net";
+$user = "jetson-admin";
+$password = "n5UMLqP!SG*b3f#";
+$database = "jetson-monitor-database";
+
+$conn = new mysqli($server, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$query = "SELECT * FROM system_stats ORDER BY insertion_time DESC LIMIT 1";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo "<p>CPU Percent: " . $row['cpu_percent'] . "%</p>";
+    echo "<p>Memory Total: " . $row['memory_total'] . " GB</p>";
+    echo "<p>Memory Available: " . $row['memory_available'] . " GB</p>";
+    echo "<p>Memory Used: " . $row['memory_used'] . " GB</p>";
+    echo "<p>Memory Free: " . $row['memory_free'] . " GB</p>";
+    echo "<p>Insertion Time: " . $row['insertion_time'] . "</p>";
+} else {
+    echo "No data available.";
+}
+
+$conn->close();
+?>
